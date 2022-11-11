e11ad8127087732ef61516bbe3a63cdd5a4b1440
kkkevinxu <908737195@qq.com>
2022-11-11 16:40:59 +0000

    config the dofilter function
    by using circle method (?) 我是天才

--- fir.py
+++ fir.py
@@ -39,31 +39,32 @@ class FIRfilter:
         # your code here 
         self.co = _coefficients
         self.save = []
-        self.h = []
-        for i in range(0,len(self.co)):
-            for m in range(0,30):
-                self.h.append(self.co[i])
+        self.taps = 1000
+        
     def dofilter(self, v): 
         result = 0
-        index = len(self.save)
         # your code here 
-        index = 0
         self.save.append(v)
-        for m in range(0,index+1):
-            result = result + self.h[index-m]*self.save[m]
+        index = len(self.save)
+        if index > self.taps:
+            index = 1
+            self.save = []
+            self.save.append(v)
+        for m in range(0,index):
+            result = result + self.co[index-1-m]*self.save[m]
         return result
     
 def main():
     data = np.loadtxt('../fft_2780667Y_2780703G_2791506X/ECG_1000Hz_43.dat')
     fs = 1000
     ts = 1/fs
-    time = np.linspace(0,len(data[0:1000])/fs,len(data))
+    time = np.linspace(0,len(data)/fs,len(data))
     plt.plot(time,data)
     plt.xlabel('time(s)')
     plt.show()
     plt.ylabel('amplitude')
     
-    data_fft = np.fft.fft(data[0:1000])
+    data_fft = np.fft.fft(data)
     fre = np.linspace(0,fs,len(data_fft))
     shiftfft = fftshift(20*np.log10(np.abs(data_fft)),len(data_fft))
     plt.plot(fre,shiftfft)
@@ -72,11 +73,11 @@ def main():
     plt.show()
     
     h1 = fftshift(highpassDesign(fs,1),fs) * np.hamming(fs)
-    y1 = signal.lfilter(h1,1,data[0:1000])
-    #fir1 = FIRfilter(h1)
-    #y1 = np.zeros(len(data))
-    #for i in range(0,len(data)):
-    #    y1[i] = fir1.dofilter(data[i])
+    #y1 = signal.lfilter(h1,1,data[0:1000])
+    fir1 = FIRfilter(h1)
+    y1 = np.zeros(len(data))
+    for i in range(0,len(data)):
+        y1[i] = fir1.dofilter(data[i])
     fft_y1 = np.fft.fft(y1)
     shifty1 = fftshift(20*np.log10(np.abs(fft_y1)),len(fft_y1))
     plt.plot(fre,shifty1)
@@ -85,11 +86,11 @@ def main():
     plt.show()
     
     h2 = fftshift(bandstopDesign(fs,45,55),fs) * np.hamming(fs)
-    y2 = signal.lfilter(h2,1,y1[0:1000])
-    #fir2 = FIRfilter(h2)
-    #y2 = np.zeros(len(data))
-    #for i in range(0,len(data)):
-    #    y2[i] = fir2.dofilter(y1[i])
+    #y2 = signal.lfilter(h2,1,y1)
+    fir2 = FIRfilter(h2)
+    y2 = np.zeros(len(data))
+    for i in range(0,len(data)):
+        y2[i] = fir2.dofilter(y1[i])
     fft_y2 = np.fft.fft(y2)
     shifty2 = fftshift(20*np.log10(np.abs(fft_y2)),len(fft_y2))
     plt.plot(fre,shifty2)
